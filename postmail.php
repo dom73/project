b1a485ef7b67eeb7ddc0d03ee88f7bc46f257d1e
dominique <dom73@users.noreply.github.com>
2022-12-30 20:07:12 +0100

    Add files via upload

--- postmail.php
+++ postmail.php
@@ -1,20 +1,35 @@
-//1 fichier ex.:formulaire.php ou formulaire.html
+<?php
+@include_once("Mail.php");
 
-<HTML>
-<HEAD>
-<TITLE>Messages</TITLE>
-</HEAD>
-<BODY>
-<FORM method="POST" action="envoi.php">
-<P>Votre nom:<br>
-<INPUT type="text" name="nom" size=30>
-</p>
-<P>Votre mail:<br>
-<INPUT type="text" name="email" size=30>
-</p>
-<P>Message:<br>
-<textarea name="message" cols=30 rows=5></textarea>
-</p><INPUT type="submit" value="Envoyer">
-</FORM>
-</BODY>
-</HTML>
+$name = $_POST["Nom"];
+$objet = $_POST["Objet"];
+$message = $_POST["Message"];
+$from = $_POST["Email"];
+
+// remove the backslashes that normally appears when entering " or '
+$name = stripslashes($name);
+$message = stripslashes($message);
+$objet = stripslashes($objet);
+$from = stripslashes($from);
+
+$to_address = "hugondo73@gmail.com";
+
+// Check to see if a message was recieved
+if(isset($message) and $message!="") {
+    // send the message and show this page
+    mail($to_address, 'Online Form: '.$objet, $_SERVER['REMOTE_ADDR']."\n\nName: ".$name."\n\n".$message, "From: $from");
+} else {
+    echo "no variables received, this page cannot be accessed directly";
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>dom73's contact</title>
+    </head>
+    <body>
+        <p>Merci de votre intérêt, nous vous répondrons au plus tôt.</p>
+    </body>
+</html>
