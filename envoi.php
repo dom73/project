ac9e8fd1af602b6eec4017bde83772f0b55d3fdc
dominique <dom73@users.noreply.github.com>
2022-12-30 20:22:15 +0100

    Add files via upload

--- envoi.php
+++ envoi.php
@@ -6,10 +6,10 @@ $msg = "Nom:\t$Nomr\n";
 $msg .= "E-mail:\t$email\n";
 $msg .= "Message:\t$message\n\n";
 
-$recipient = "hugondo73@gmail.com";
+$recipient = "hugondo73@mail.com";
 $subject = "Formulaire";
 
-$mailheaders = "From: hugondo73.free.fr<> \n";
+$mailheaders = "From: Mon site web<> \n";
 $mailheaders .= "Reply-To: $CP\n\n";
 
 mail($recipient, $subject, $msg, $mailheaders);
@@ -18,7 +18,7 @@ echo "<HTML><HEAD>";
 echo "<TITLE>Formulaire envoyer!</TITLE></HEAD><BODY>";
 echo "<H1 align=center>Merci, $nom </H1>";
 echo "<P align=center>";
-echo "Votre formulaire Ã  bien Ã©tÃ© envoyÃ© !</P>";
+echo "Votre formulaire à bien été envoyé !</P>";
 echo "</BODY></HTML>";
 
 ?> 
